f53b4dddd3921f71bc31352aaf8592f93af9d29e
Raveendiran RR <smartbond@gmail.com>
2019-11-14 16:04:09 +0530

    testing finally

--- Configure_Slave.py
+++ Configure_Slave.py
@@ -1,13 +1,13 @@
 # Add or remove the jenkins slave using this python program
 import jenkins 
 
-server = jenkins.Jenkins('http://10.128.0.10:8080', username='admin', password='697861f3a07c455b8486531a61f7009c')
+server = jenkins.Jenkins('http://35.222.8.166/:8080', username='admin', password='697861f3a07c455b8486531a61f7009c')
 # create node with parameters
 params = {
     'port': '22',            # SSH Port
     'username': 'Jenkins',   # Credential where the private key of the master is stored
     'credentialsId': '2',    # Id of the credential
-    'host': '104.155.131.85' # using external IP addresss coz trying this from my laptop. Use internal IP address 
+    'host': '35.193.93.151'   # using external IP addresss coz trying this from my laptop. Use internal IP address 
 }
 
 # create a new node with the following parameters
@@ -28,3 +28,4 @@ print("number of node is ",nodes)
 
 #server.disable_node('slave',msg="test the disable thing") # name = slave1
 #server.delete_node('slave')
+
